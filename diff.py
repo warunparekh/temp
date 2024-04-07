38cd3b6fa632f2ab76f62059a992338e9de943e8
warun parekh <ichiko1503@gmail.com>
2024-04-07 14:18:15 +0530

    addnig diff.py

---
+++ diff.py
@@ -0,0 +1,13 @@
+# Function to calculate the sum of two numbers
+def sum_of_two_numbers(num1, num2):
+    return num1 + num2
+
+# Input
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+result = sum_of_two_numbers(number1, number2)
+
+# Display the result
+print("The sum of", number1, "and", number2, "is", result)
